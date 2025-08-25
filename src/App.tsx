000572c6979649382c4115f2c1ca90f0713762a1
Amadou Bathily <amadouba7h@gmail.com>
2025-08-25 12:35:12 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,9 @@ import PlanSitePage from './pages/Parametrages/plan-site/PlanSitePage'
 import TypeZonePage from './pages/Parametrages/type-zone/TypeZonePage'
 import Localites from './pages/Parametrages/Localites'
 import PartFinanciers from './pages/Parametrages/PartFinancier'
-import ConventionPage from './pages/Parametrages/convention/ConventionPage'
 import ChangePassword from './pages/ChangePassword'
 import Ugls from './pages/Parametrages/Ugl'
-import Ugls from './pages/Parametrages/Ugl'
 import ConventionPage from './pages/Parametrages/convention/ConventionPage'
-import ChangePassword from './pages/ChangePassword'
 
 const AppContent: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true)
@@ -86,7 +83,7 @@ const AppContent: React.FC = () => {
             element={isAuthenticated ? <Acteurs /> : <Navigate to="/login" />}
           />
           <Route
-            path="/personnel"
+            path="/utilisateurs"
             element={isAuthenticated ? <PersonnelPage /> : <Navigate to="/login" />}
           />
           <Route
@@ -98,15 +95,15 @@ const AppContent: React.FC = () => {
             element={isAuthenticated ? <Ugls /> : <Navigate to="/login" />}
           />
           <Route
-            path="/plan-site"
+            path="/plan-sites"
             element={isAuthenticated ? <PlanSitePage /> : <Navigate to="/login" />}
           />
           <Route
-            path="/type-zone"
+            path="/type-zones"
             element={isAuthenticated ? <TypeZonePage /> : <Navigate to="/login" />}
           />
           <Route
-            path="/convention"
+            path="/conventions"
             element={isAuthenticated ? <ConventionPage /> : <Navigate to="/login" />}
           />
           <Route
