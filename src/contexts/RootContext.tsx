183425f6fae1c3323c73b47fb5a624e7afa6a71c
Mohamed83834515 <mohamedmoustaphatr@gmail.com>
2025-08-28 00:29:19 +0000

    niveauStructures

--- src/contexts/RootContext.tsx
+++ src/contexts/RootContext.tsx
@@ -1,70 +1,75 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { Programme } from "../types/programme";
-import { getAllProgrammes } from "../functions/programme";
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { Programme } from '../types/programme'
+import { getAllProgrammes } from '../functions/Programme/gets'
 
 interface contextType {
-    currentProgramme: Programme | undefined;
-    setCurrentProgramme: React.Dispatch<React.SetStateAction<Programme | undefined>>;
-    programmeList: Programme[];
-    setprogrammeList: React.Dispatch<React.SetStateAction<Programme[]>>;
+  currentProgramme: Programme | undefined
+  setCurrentProgramme: React.Dispatch<
+    React.SetStateAction<Programme | undefined>
+  >
+  programmeList: Programme[]
+  setprogrammeList: React.Dispatch<React.SetStateAction<Programme[]>>
 }
 
 const RootContext = createContext<contextType | undefined>(undefined)
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
-    const [currentProgramme, setCurrentProgramme] = useState<Programme | undefined>(undefined);
-    const [programmeList, setprogrammeList] = useState<Programme[]>([]);
+  const [currentProgramme, setCurrentProgramme] = useState<
+    Programme | undefined
+  >(undefined)
+  const [programmeList, setprogrammeList] = useState<Programme[]>([])
 
-    const loadProject = async () => {
-        try {
-            const res = await getAllProgrammes();
-            res && setprogrammeList(res);
-        } catch (error) { }
+  const loadProject = async () => {
+    try {
+      const res = await getAllProgrammes()
+      res && setprogrammeList(res)
+    } catch (error) {}
+  }
+
+  const initCUrrentProject = async () => {
+    const programme = localStorage.getItem('currentProgramme')
+
+    if (!programme) {
+      programmeList.length && setCurrentProgramme(programmeList[0])
+      return
     }
 
-    const initCUrrentProject = async () => {
-        const programme = localStorage.getItem('currentProgramme');
+    setCurrentProgramme(JSON.parse(programme))
+  }
 
-        if (!programme) {
-            programmeList.length && setCurrentProgramme(programmeList[0]);
-            return;
-        }
+  useEffect(() => {
+    loadProject()
+  }, [])
 
-        setCurrentProgramme(JSON.parse(programme));
-    }
+  useEffect(() => {
+    initCUrrentProject()
+  }, [programmeList])
 
-    useEffect(() => {
-        loadProject();
-    }, []);
+  useEffect(() => {
+    currentProgramme &&
+      localStorage.setItem('currentProgramme', JSON.stringify(currentProgramme))
+  }, [currentProgramme])
 
-    useEffect(() => {
-        initCUrrentProject();
-    }, [programmeList]);
-
-    useEffect(() => {
-        currentProgramme && localStorage.setItem("currentProgramme", JSON.stringify(currentProgramme));
-    }, [currentProgramme]);
-
-
-    return (
-        <RootContext.Provider value={{
-            currentProgramme,
-            setCurrentProgramme,
-            programmeList,
-            setprogrammeList,
-        }
-        }>
-            {children}
-        </RootContext.Provider>
-    )
+  return (
+    <RootContext.Provider
+      value={{
+        currentProgramme,
+        setCurrentProgramme,
+        programmeList,
+        setprogrammeList,
+      }}
+    >
+      {children}
+    </RootContext.Provider>
+  )
 }
 
 export const useRoot = (): contextType => {
-    const context = useContext(RootContext);
+  const context = useContext(RootContext)
 
-    if (context === undefined) {
-        throw new Error("useRoot must be used within an RootProvider");
-    }
+  if (context === undefined) {
+    throw new Error('useRoot must be used within an RootProvider')
+  }
 
-    return context;
-};
+  return context
+}
--- src/functions/programme/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as getAllProgrammes } from "./gets"
