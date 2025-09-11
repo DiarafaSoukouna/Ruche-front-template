73940a9d02199e34448aa5c3202dec63df38777b
Mohamed83834515 <mohamedmoustaphatr@gmail.com>
2025-09-11 15:58:27 +0000

    oklm

--- src/App.tsx
+++ src/App.tsx
@@ -29,28 +29,9 @@ import PlanSitePage from './pages/Parametrages/planSite'
 import FonctionPage from './pages/Parametrages/personnel/fonction-personnel/FonctionPage'
 import Programmes from './pages/Parametrages/Programme'
 import CadreAnalytique from './pages/CadreAnalytique'
-} from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { AuthProvider } from "./contexts/AuthContext";
-import LoadingScreen from "./components/LoadingScreen";
-import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Users from "./pages/Users";
-import Products from "./pages/Products";
-import Analytics from "./pages/Analytics";
-import Acteurs from "./pages/Parametrages/Acteurs/index";
-import PersonnelPage from "./pages/Parametrages/personnel/PersonnelPage";
-import PlanSitePage from "./pages/Parametrages/plan-site/PlanSitePage";
-import TypeZonePage from "./pages/Parametrages/type-zone/TypeZonePage";
-import Localites from "./pages/Parametrages/Localites";
-import PartFinanciers from "./pages/Parametrages/PartFinancier";
-import ChangePassword from "./pages/ChangePassword";
-import Ugls from "./pages/Parametrages/Ugl";
-import ConventionPage from "./pages/Parametrages/convention/ConventionPage";
-import NiveauStructureConfigPage from "./pages/Parametrages/niveau-structure-config/NiveauStructureConfigPage";
-
+import Referentiels from './pages/Parametrages/Refenrentiels'
+import Projet from './pages/Projet'
+import { RootProvider } from './contexts/RootContext'
 const AppContent: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true)
   const isAuthenticated = useState<boolean>(true)
